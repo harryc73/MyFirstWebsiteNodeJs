7402a3f8791dfa766fa457e0f9e97276f194a78a
Harry C <124547151+harryc73@users.noreply.github.com>
2024-10-20 09:44:15 +0000

    Site up and running

---
+++ public/js/app.js
@@ -0,0 +1,54 @@
+let result = "";
+fetch("./frontEndData.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log("error: " + err);
+  });
+function appendData(data) {
+  data.forEach(({ name, image, hyperlink, about, language } = rows) => {
+    result += `
+        <div class="card">
+        <img class="card-image" src="${image}" alt="Product image for the ${name} VSCode extension."/>
+        <h1 class="card-name">${name}</h1>
+        <p class="card-about">${about}</p>
+        <a class="card-link" href="${hyperlink}"><button class="btn">Read More</button></a>
+        </div>
+        `;
+  });
+  document.querySelector(".container").innerHTML = result;
+}
+
+if ("serviceworker" in navigator) {
+  window.addEventListener("load", function () {
+    navigator.serviceworker
+      .register("js/serviceworker.js")
+      .then((res) => console.log("service worker registered"))
+      .catch((err) => console.log("service worker not registered", err));
+  });
+}
+
+app.post("/add.html", function (req, res) {
+  db.serialize(() => {
+    db.run(
+      "INSERT INTO email(email,name) VALUES(?,?)",
+      [req.body.email, req.body.name],
+      function (err) {
+        if (err) {
+          return console.log(err.message);
+        }
+        res.send(
+          "Thank you " +
+            req.body.name +
+            " we have added your email " +
+            req.body.email +
+            " to our distribution list."
+        );
+      }
+    );
+  });
+});
